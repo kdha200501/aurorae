2274956217256e916b0a1913f59291f8ddb41c05
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2025-11-19 16:20:26 +0200

    Fix wrong paths in Messages.sh

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find src themes -name \*.kcfg -o -name \*.ui` >> rc.cpp || exit 11
-$XGETTEXT rc.cpp `find src themes -name \*.cpp -o -name \*.qml` -o $podir/aurorae.pot
+$EXTRACTRC `find . -name \*.kcfg -o -name \*.ui` >> rc.cpp || exit 11
+$XGETTEXT rc.cpp `find . -name \*.cpp -o -name \*.qml` -o $podir/aurorae.pot
 rm -f rc.cpp
